23e6969586615f4b8de3f9de038f33202ad7adb4
unknown <catislove@kangwon.ac.kr>
2024-09-07 22:33:23 +0900

    First commit

---
+++ src/ageandgrade.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class ageandgrade {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int age;
+        double grade;
+        System.out.println("나이를 입력해주십시오.");
+    }
+}
