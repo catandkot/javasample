9657fcfde6eafefc0cbaa26bcca06607d2c3db82
aeongs <aeong@catislove.kr>
2024-09-15 13:40:49 +0900

    번호 수정

---
+++ src/fourth/ruleAdd.java
@@ -0,0 +1,11 @@
+package fourth;
+
+// 6주차 과제 7번.
+// 다음과 같은 순서의 숫자들은 어떤 논리적 순서에 의해 나열되어 있다.
+// 이 순서를 만족하는 숫자 15개를 출력하는 프로그램을 작성하시오. 아래 문제는 숫자를 1씩 증가시켜 그 전 값에 더하여 출력하고 있다.
+
+public class ruleAdd {
+    public static void main(String[] args) {
+        int count, sum;
+    }
+}
